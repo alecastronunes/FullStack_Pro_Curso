53e380d6e8e8abe001b3d3992acf3d077d0404c1
alecastronunes <alexandre.castro92@gmail.com>
2025-05-20 17:45:10 -0300

    Subindo funcoes de adicionar e excluir.

--- modulo_javascript/projetos/lista_tarefas/script/script.js
+++ modulo_javascript/projetos/lista_tarefas/script/script.js
@@ -4,6 +4,30 @@ let buttonElement = document.querySelector("#app button");
 
 let todoList = [];
 
+function renderTodo() {
+  listElement.innerHTML = "";
+
+  todoList.map((todo) => {
+    // Cria os elementos e os adiciona dentro da <ul></ul>.
+    let liElement = document.createElement("li");
+    let textTodo = document.createTextNode(todo);
+
+    let linkElement = document.createElement("a");
+    linkElement.setAttribute("href", "#");
+
+    let position = todoList.indexOf(todo);
+
+    linkElement.setAttribute("onclick", `deleteTodo(${position})`);
+
+    let linkText = document.createTextNode("Excluir");
+    linkElement.appendChild(linkText);
+
+    listElement.appendChild(liElement);
+    liElement.appendChild(textTodo);
+    liElement.appendChild(linkElement);
+  });
+}
+
 function addTarefas() {
   if (inputElement.value === "") {
     alert("Digite alguma tarefa!!!");
@@ -11,8 +35,14 @@ function addTarefas() {
   } else {
     let newTodo = inputElement.value;
     todoList.push(newTodo);
-    console.log(todoList);
+    inputElement.value = "";
+    renderTodo();
   }
 }
 
 buttonElement.onclick = addTarefas;
+
+function deleteTodo(position) {
+  todoList.splice(position, 1);
+  renderTodo();
+}
