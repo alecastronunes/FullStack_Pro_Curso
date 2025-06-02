2beb73573128d38e67d9a12c1436a8658f067eaa
Alexandre de Castro Nunes <alexandre.castro92@gmail.com>
2025-06-02 15:31:27 -0300

    Subindo arquivos sobre metodos de busca em Js e requisicoes http

---
+++ modulo_javascript/metodos_busca.js
@@ -0,0 +1,14 @@
+// Includes, startsWith, endsWith
+
+//let nomes = ["Boruto", "Minato", "Itachi", "Hashirama"];
+
+// if (nomes.includes("Sassuke")) {
+//   console.log("O nome consta na lista!!!");
+// } else {
+//   console.log("O nome não consta na lista!!!");
+// }
+
+let usuario = "Naruto"
+// console.log(usuario.startsWith("N"));
+
+console.log(usuario.endsWith("ruto"));
