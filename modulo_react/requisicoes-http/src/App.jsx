c7ae6bb5efa8ab124f3372f9e90f459239d1abfd
Alexandre de Castro Nunes <alexandre.castro92@gmail.com>
2025-06-23 10:29:50 -0300

    http requisicao

---
+++ modulo_react/requisicoes-http/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+//https://sujeitoprogramador.com/rn-api/?api=posts
+function App() {
+  const [nutri, setNutri] = useState([]);
+
+  useEffect(() => {
+    function loadApi() {
+      const url = "https://sujeitoprogramador.com/rn-api/?api=posts";
+      fetch(url)
+        .then((r) => r.json())
+        .then((json) => {
+          setNutri(json);
+        });
+    }
+
+    loadApi();
+  }, []);
+
+  return (
+    <div className="container">
+      <header>React Nutri</header>
+
+      {nutri.map((post) => {
+        return (
+          <article key={post.id} className="post">
+            <strong className="titulo">{post.titulo}</strong>
+            <img src={post.capa} alt={post.titulo} className="capa" />
+            <p className="subtitulo">{post.subtitulo}</p>
+            <a className="botao">Acessar</a>
+          </article>
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
